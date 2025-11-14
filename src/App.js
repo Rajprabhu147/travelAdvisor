bda91c175646c55381f1622f884137211f692979
KingLord147 <rajprabhu.147@gmail.com>
2025-11-14 18:59:02 +0530

    Api Fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { CssBaseline, Grid } from '@material-ui/core';
+import React, { useEffect, useState } from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
 
-import { getPlacesData } from './api';
+import { getPlacesData } from "./api";
 
-import Header from './components/Header/Header';
-import List from './components/List/List';
-import Map from './components/Map/Map';
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 const App = () => {
-  const [type, setType] = useState('restaurants');
-  const [rating, setRating] = useState('');
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
 
   const [coordinates, setCoordinates] = useState({});
-  const [bounds, setBounds] = useState({ sw: 0, ne: 0});
-
+  const [bounds, setBounds] = useState({ sw: 0, ne: 0 });
 
   const [places, setPlaces] = useState([]);
   const [filteredPlaces, setFilteredPlaces] = useState([]);
@@ -24,9 +23,11 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
-      setCoordinates({ lat: latitude, lng: longitude });
-    })
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
   }, []);
 
   useEffect(() => {
@@ -36,48 +37,55 @@ const App = () => {
   }, [rating]);
 
   useEffect(() => {
-    if(bounds.sw && bounds.ne) {
+    if (bounds.sw && bounds.ne) {
       setIsLoading(true);
 
-
       getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
-          setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
-          setFilteredPlaces([]);
-          setIsLoading(false);
-        })
+        setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setIsLoading(false);
+      });
     }
   }, [type, coordinates, bounds]);
 
-  // const onLoad = (autoC) => setAutocomplete(autoC);
+  const onLoad = (autoC) => setAutocomplete(autoC);
 
-  // const onPlaceChanged = () => {
-  //     const lat = autocomplete.getPlace().geometry.location.lat();
-  //     const lng = autocomplete.getPlace().geometry.location.lng();
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
 
-  //     setCoordinates({lat, lng});
-  // }
+    setCoordinates({ lat, lng });
+  };
 
   return (
     <>
       <CssBaseline />
       <Header />
-      <Grid container spacing={3} style={{ width: '100%' }}>
+      <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
           <List
-            isLoading={isLoading} 
+            isLoading={isLoading}
             childClicked={childClicked}
-            places={filteredPlaces.length ? filteredPlaces : places} 
+            places={filteredPlaces.length ? filteredPlaces : places}
             type={type}
             setType={setType}
             rating={rating}
             setRating={setRating}
-
           />
         </Grid>
-        <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alingItems: 'center' }}>
-          <Map 
-            setCoordinates={setCoordinates} 
-            setBounds={setBounds} 
+        <Grid
+          item
+          xs={12}
+          md={8}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alingItems: "center",
+          }}
+        >
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
             coordinates={coordinates}
             places={filteredPlaces.length ? filteredPlaces : places}
             setChildClicked={setChildClicked}
@@ -86,7 +94,7 @@ const App = () => {
         </Grid>
       </Grid>
     </>
-  )
-}
+  );
+};
 
 export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,57 @@
 import axios from "axios";
-  
+
 export const getPlacesData = async (type, sw, ne) => {
-    try {
-        const { data: { data }} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
-          params: {
-            bl_latitude: sw.lat,
-            tr_latitude: ne.lat,
-            bl_longitude: sw.lng,
-            tr_longitude: ne.lng,
-          },
-          headers: {
-            'X-RapidAPI-Key': '4e41846358msh101a8af3de8c87fp19545ejsne31b2e4e2680',
-            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-          }
-        });
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "X-RapidAPI-Key":
+            "f0ad403f32msh97c5fd3b16a3813p197c96jsnde8b8fe2cf0e",
+          "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
 
-        return data;
-    } catch (error) {
+    return data;
+  } catch (error) {}
+};
 
-    }
-}
+// import axios from 'axios';
 
+// const options = {
+//   method: 'GET',
+//   url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
+//   params: {
+//     bl_latitude: '11.847676',
+//     tr_latitude: '12.838442',
+//     bl_longitude: '109.095887',
+//     tr_longitude: '109.149359',
+//     restaurant_tagcategory_standalone: '10591',
+//     restaurant_tagcategory: '10591',
+//     limit: '30',
+//     currency: 'USD',
+//     open_now: 'false',
+//     lunit: 'km',
+//     lang: 'en_US'
+//   },
+//   headers: {
+//     'x-rapidapi-key': 'f0ad403f32msh97c5fd3b16a3813p197c96jsnde8b8fe2cf0e',
+//     'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
+//   }
+// };
+
+// try {
+// 	const response = await axios.request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
