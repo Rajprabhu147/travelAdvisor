7aba5a7819972033da522a7f50510268fd5c6363
KingLord147 <rajprabhu.147@gmail.com>
2025-11-16 21:10:48 +0530

    Adding Logo png

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,86 +1,92 @@
-import React, { useState, useEffect, createRef } from "react";
-import {
-  CircularProgress,
-  Grid,
-  Typography,
-  InputLabel,
-  MenuItem,
-  FormControl,
-  Select,
-} from "@material-ui/core";
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating";
 
-import PlaceDetails from "../PlaceDetails/PlaceDetails";
+import mapStyles from "../../mapStyles";
 import useStyles from "./styles.js";
 
-const List = ({
+const Map = ({
+  coords,
   places,
-  type,
-  setType,
-  rating,
-  setRating,
-  childClicked,
-  isLoading,
+  setCoords,
+  setBounds,
+  setChildClicked,
+  weatherData,
 }) => {
-  const [elRefs, setElRefs] = useState([]);
+  const matches = useMediaQuery("(min-width:600px)");
   const classes = useStyles();
 
-  useEffect(() => {
-    setElRefs((refs) =>
-      Array(places.length)
-        .fill()
-        .map((_, i) => refs[i] || createRef())
-    );
-  }, [places]);
-
   return (
-    <div className={classes.container}>
-      <Typography variant="h4">Food & Dining around you</Typography>
-      {isLoading ? (
-        <div className={classes.loading}>
-          <CircularProgress size="5rem" />
-        </div>
-      ) : (
-        <>
-          <FormControl className={classes.formControl}>
-            <InputLabel id="type">Type</InputLabel>
-            <Select
-              id="type"
-              value={type}
-              onChange={(e) => setType(e.target.value)}
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: mapStyles,
+        }}
+        onChange={(e) => {
+          setCoords({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => setChildClicked(child)}
+      >
+        {places.length &&
+          places.map((place, i) => (
+            <div
+              className={classes.markerContainer}
+              lat={Number(place.latitude)}
+              lng={Number(place.longitude)}
+              key={i}
             >
-              <MenuItem value="restaurants">Restaurants</MenuItem>
-              <MenuItem value="hotels">Hotels</MenuItem>
-              <MenuItem value="attractions">Attractions</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl className={classes.formControl}>
-            <InputLabel id="rating">Rating</InputLabel>
-            <Select
-              id="rating"
-              value={rating}
-              onChange={(e) => setRating(e.target.value)}
-            >
-              <MenuItem value="">All</MenuItem>
-              <MenuItem value="3">Above 3.0</MenuItem>
-              <MenuItem value="4">Above 4.0</MenuItem>
-              <MenuItem value="4.5">Above 4.5</MenuItem>
-            </Select>
-          </FormControl>
-          <Grid container spacing={3} className={classes.list}>
-            {places?.map((place, i) => (
-              <Grid ref={elRefs[i]} key={i} item xs={12}>
-                <PlaceDetails
-                  selected={Number(childClicked) === i}
-                  refProp={elRefs[i]}
-                  place={place}
-                />
-              </Grid>
-            ))}
-          </Grid>
-        </>
-      )}
+              {!matches ? (
+                <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              ) : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography
+                    className={classes.typography}
+                    variant="subtitle2"
+                    gutterBottom
+                  >
+                    {" "}
+                    {place.name}
+                  </Typography>
+                  <img
+                    className={classes.pointer}
+                    src={
+                      place.photo
+                        ? place.photo.images.large.url
+                        : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                    }
+                  />
+                  <Rating
+                    name="read-only"
+                    size="small"
+                    value={Number(place.rating)}
+                    readOnly
+                  />
+                </Paper>
+              )}
+            </div>
+          ))}
+        {weatherData?.list?.length &&
+          weatherData.list.map((data, i) => (
+            <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+              <img
+                src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+                height="70px"
+              />
+            </div>
+          ))}
+      </GoogleMapReact>
     </div>
   );
 };
 
-export default List;
+export default Map;
